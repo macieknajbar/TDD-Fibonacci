0b069233463f61bb7a36215fb654ac029b4a9421
Maciej Najbar <maciek.najbar@gmail.com>
2019-02-26 23:48:21 +0100

    Calculate fib presenter

--- app/src/main/java/com/gmail/najbar/maciek/tdd/results/ResultsActivity.kt
+++ app/src/main/java/com/gmail/najbar/maciek/tdd/results/ResultsActivity.kt
@@ -3,11 +3,14 @@ package com.gmail.najbar.maciek.tdd.results
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.gmail.najbar.maciek.tdd.R
+import com.gmail.najbar.maciek.tdd.results.presenter.CalculateFibonacciPresenter
 import com.gmail.najbar.maciek.tdd.results.usecase.CalculateFibonacci
+import com.gmail.najbar.maciek.tdd.results.usecase.CalculateFibonacciImpl
 
 class ResultsActivity : AppCompatActivity() {
 
-    private lateinit var calculateFibonacci: CalculateFibonacci
+    private val calculateFibonacci: CalculateFibonacci =
+            CalculateFibonacciImpl(CalculateFibonacciPresenter())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/gmail/najbar/maciek/tdd/results/presenter/CalculateFibonacciPresenter.kt
@@ -0,0 +1,10 @@
+package com.gmail.najbar.maciek.tdd.results.presenter
+
+import com.gmail.najbar.maciek.tdd.results.usecase.CalculateFibonacci
+
+class CalculateFibonacciPresenter : CalculateFibonacci.Presenter {
+
+    override fun presentResults(fib: Int) {
+
+    }
+}
