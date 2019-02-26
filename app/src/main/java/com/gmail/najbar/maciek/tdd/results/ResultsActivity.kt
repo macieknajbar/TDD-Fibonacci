ade8bd7f23f5dbf2be372c43fa0dcee6c0e90255
Maciej Najbar <maciek.najbar@gmail.com>
2019-02-26 23:16:25 +0100

    Results title is displayed

--- app/src/main/java/com/gmail/najbar/maciek/tdd/results/ResultsActivity.kt
+++ app/src/main/java/com/gmail/najbar/maciek/tdd/results/ResultsActivity.kt
@@ -1,8 +1,15 @@
 package com.gmail.najbar.maciek.tdd.results
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.gmail.najbar.maciek.tdd.R
 
 class ResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+    }
 
     companion object {
         const val EXTRA_NUMBER = "EXTRA_NUMBER"
